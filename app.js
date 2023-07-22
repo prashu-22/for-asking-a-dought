f23fd688f436a89629e456a33cc51e4ae854dc0a
prashu-22 <eerantiprashanth2@gmail.com>
2023-07-22 08:49:53 +0000

    commit message

--- app.js
+++ app.js
@@ -13,46 +13,46 @@ const dbPath = path.join(__dirname, "userData.db");
 let db = null;
 
 const initialization = async () => {
-    try {
-        db = await open({
-            filename: dbPath,
-            driver: sqlite3.Database,
-        });
-        app.listen(3000, () => {
-            console.log("server Running at localhost");
-        });
-    } catch (e) {
-        console.log(`error at ${e.message}`);
-        process.exit(1);
-    }
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server Running at localhost");
+    });
+  } catch (e) {
+    console.log(`error at ${e.message}`);
+    process.exit(1);
+  }
 };
 
 initialization();
 
 app.post("/register/", async (request, response) => {
-    const { username, name, password, gender, location } = request.body;
-    const hashedPassword = await bcrypt.hash(password, 20);
-    const userQuery = `SELECT * FROM user WHERE username = '${username}'`;
-    const dbResult = await db.get(userQuery);
+  const { username, name, password, gender, location } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const userQuery = `SELECT * FROM user WHERE username = '${username}'`;
+  const dbResult = await db.get(userQuery);
 
-    if (dbResult === undefined) {
-        let length = password.length();
-        if (length < 5) {
-            response.status(400);
-            response.send("Password is too short");
-        } else {
-            const createUserQuery = `INSERT INTO user(username,name,password,gender,location) VALUES 
+  if (dbResult === undefined) {
+    let length = password.length();
+    if (length < 5) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      const createUserQuery = `INSERT INTO user(username,name,password,gender,location) VALUES 
             (${username},
             ${name},${hashedPassword},${gender},${location});
             `;
-            const created = await db.run(createUserQuery);
-            response.status(200);
-            response.send("User created successfully");
-        }
-    } else {
-        response.status(400);
-        response.send("User already exists");
+      const created = await db.run(createUserQuery);
+      response.status(200);
+      response.send("User created successfully");
     }
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
 });
 
 module.exports = app;
